e60ae765c67344f3bc0b726baa9dc048bcede11a
henrikuiuc <157164155+henrikuiuc@users.noreply.github.com>
2026-05-03 19:58:03 -0500

    Update master.py

--- master.py
+++ master.py
@@ -1,1 +1,3 @@
+# This is the master file for the whole project. This file is intended to run the entire project workflow, with the only necessary external file being an API key.
+# As such, the file must be ran in the same folder as a .txt with a FRED API key, which can be acquired for free. 
 
