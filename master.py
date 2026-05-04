462b3ab21025ab99dae562d0492b7f8a1323efdb
henrikuiuc <157164155+henrikuiuc@users.noreply.github.com>
2026-05-03 20:01:55 -0500

    Update master.py

--- master.py
+++ master.py
@@ -1,3 +1,8 @@
 # This is the master file for the whole project. This file is intended to run the entire project workflow, with the only necessary external file being an API key.
 # As such, the file must be ran in the same folder as a .txt with a FRED API key, which can be acquired for free. 
 
+import pandas as pd
+import requests
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
